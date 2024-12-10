b385b794130b3c89b425ddf076ffc99f3812b579
[SkySnkrs] <[skysnkrs@gmail.com]>
2024-12-10 15:05:17 -0700

    init

---
+++ client/src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const useSockets = false
+export const domain = 'dev-62jltglpyp8sjjmw.us.auth0.com'
+export const clientId = 'vhDhdABLWXYl58A8ff8NcCIVawcHrgqF'
+export const audience = 'https://auth.projectapi.com'
