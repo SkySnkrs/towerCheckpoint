64c4fbb4a69629506fe0bfae4d3beafd8274f0d4
[SkySnkrs] <[skysnkrs@gmail.com]>
2024-12-11 12:00:22 -0700

    added floating cards fixed some nav styling

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,19 +1,10 @@
 <script setup>
 import { onBeforeUnmount, onMounted, ref } from 'vue';
-import { loadState, saveState } from '../utils/Store.js';
 import Login from './Login.vue';
 
-const theme = ref(loadState('theme') || 'light')
 
-onMounted(() => {
-  document.documentElement.setAttribute('data-bs-theme', theme.value)
-})
 
-function toggleTheme() {
-  theme.value = theme.value == 'light' ? 'dark' : 'light'
-  document.documentElement.setAttribute('data-bs-theme', theme.value)
-  saveState('theme', theme.value)
-}
+
 
 
 const screenWidth = ref(window.innerWidth);
@@ -30,14 +21,19 @@ onBeforeUnmount(() => {
   window.removeEventListener('resize', updateScreenWidth);
 });
 
+
+
 </script>
 
 <template>
-  <nav class="navbar navbar-expand-sm navbar-dark bg-dark px-3">
+  <nav class="navbar navbar-expand-sm navbar-dark bg-dark px-3"
+    :style="screenWidth <= 576 ? 'height: auto;' : 'height: 10dvh;'">
     <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
-      <div class="d-flex flex-column align-items-center ms-1">
+      <div class="d-flex">
         <img v-if="screenWidth >= 576" class="mt-5" alt="logo" src="../assets/img/giraffe.gif" height="125"
           id="navImage" />
+        <img v-else alt="logo" src="../assets/img/giraffe.gif" height="80" id="navImage" />
+
       </div>
     </router-link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
@@ -47,12 +43,7 @@ onBeforeUnmount(() => {
     <div class="collapse navbar-collapse" id="navbarText">
       <ul class="navbar-nav me-auto">
       </ul>
-      <div>
-        <button class="btn text-light" @click="toggleTheme"
-          :title="`Enable ${theme == 'light' ? 'dark' : 'light'} theme.`">
-          <Icon :name="theme == 'light' ? 'weather-sunny' : 'weather-night'" />
-        </button>
-      </div>
+
       <Login />
     </div>
   </nav>
@@ -67,20 +58,18 @@ a:hover {
   text-transform: uppercase;
 }
 
+nav {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 100;
+}
+
 
 .navbar-nav .router-link-exact-active {
   border-bottom: 2px solid var(--bs-success);
   border-bottom-left-radius: 0;
   border-bottom-right-radius: 0;
-}
-
-@media screen and (min-width: 576px) {
-  nav {
-    height: 64px;
-  }
-
-  .d-none {
-    display: flex;
-  }
 }
 </style>
--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,37 +1,138 @@
 <script setup>
+import { computed, onBeforeUnmount, onMounted, ref } from 'vue';
 
+
+const screenWidth = ref(window.innerWidth);
+
+function updateScreenWidth() {
+  screenWidth.value = window.innerWidth;
+}
+
+onMounted(() => {
+  window.addEventListener('resize', updateScreenWidth);
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener('resize', updateScreenWidth);
+});
+
+const backgroundHeight = computed(() => {
+  return screenWidth.value <= 576 ? '90dvh' : '90dvh';
+});
+
+const marginTop = computed(() => {
+  return screenWidth.value >= 768 ? '' : '2em';
+});
 </script>
 
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div class="home-card p-5 card align-items-center shadow rounded elevation-3">
-      <img src="@/assets/img/cw-circle-logo.png" alt="CodeWorks Logo"
-        class="rounded-circle">
-      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
-        Vue 3 Starter
-      </h1>
+  <section class="background-section" :style="{ height: backgroundHeight }">
+    <div class="content-container row justify-content-center align-content-center">
+      <div class="text-center col-sm-12 w-sm-50 w-75">
+        <h4 class="fw-bold text-light">Make, Create, And Enjoy Events With Tower!</h4>
+      </div>
+      <div class="text-center col-sm-12 w-75">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore eos aperiam, quod impedit est fugit
+          delectus id non omnis fugiat provident blanditiis sed commodi perferendis culpa? Eveniet neque sequi quasi.
+        </p>
+      </div>
     </div>
-  </div>
+  </section>
+
+  <section class="container" style=" position: relative; margin-top: -75px;">
+    <div class="floating-card row justify-content-between d-flex" style="background-color: transparent">
+      <div class="col-md-5 align-self-center">
+        <div class="card">
+          <div class="card-body text-light">
+            <h5 class="card-title ">Discover Events You're Interested In</h5>
+            <p class="card-text mt-4">Browse through community hosted events for all of the things you love!</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-xl-5 col-md-6  align-self-center">
+        <div class="card" :style="{ marginTop: marginTop }">
+          <div class="card-body text-light">
+            <h5 class="card-title ">Start An Event To Invite Your Friends!</h5>
+            <p class="card-text">Create your own tower event, invite your friends, and the whole tower community!</p>
+            <div class="text-end mt-3">
+              <button class="btn btn-success">Create Event</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="container event-body" style="height: 100dvh; background-color: rgb(33 37 41);">
+    <h2>Explore Top Categories:</h2>
+    <div class="ms-5 row">
+      <div class="col-3 p-3 text-center">Event</div>
+      <div class="col-3 p-3 text-center">Event</div>
+      <div class="col-3 p-3 text-center">Event</div>
+      <div class="col-3 p-3 text-center">Event</div>
+    </div>
+  </section>
 </template>
 
 <style scoped lang="scss">
-.home {
-  display: grid;
-  height: 80vh;
-  place-content: center;
-  text-align: center;
-  user-select: none;
+.background-section {
+  position: relative;
+  height: 100vh;
+  background: url('https://static.vecteezy.com/system/resources/previews/029/332/632/non_2x/ai-generative-crowded-concert-stage-scenery-with-spotlights-and-colored-lights-realistic-image-ultra-hd-free-photo.jpg') center center no-repeat;
+  background-size: cover;
+  overflow: hidden;
 
-  .home-card {
-    width: clamp(500px, 50vw, 100%);
-
-    >img {
-      height: 200px;
-      max-width: 200px;
-      width: 100%;
-      object-fit: contain;
-      object-position: center;
-    }
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.5);
+    z-index: 1;
   }
+}
+
+.content-container {
+  position: relative;
+  z-index: 2;
+}
+
+section {
+  text-shadow: 1px 1px black;
+  color: white;
+}
+
+section>div {
+  height: 100%;
+}
+
+.floating-card {
+  position: absolute;
+  top: 80%;
+  left: 50%;
+  transform: translateX(-50%);
+  z-index: 10;
+  margin-top: -50px;
+  width: 80%;
+  min-height: 50dvh;
+}
+
+.card {
+  background-color: #42474d;
+  padding: 1em;
+  min-height: 85%;
+  border-radius: 10px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+}
+
+.container {
+  position: relative;
+}
+
+.event-body {
+  margin-top: 20rem;
 }
 </style>
